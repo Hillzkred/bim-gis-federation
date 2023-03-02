bb5806f1504267c10e085fad21a6237fa577bcbf
Alexis Almacin <17034756+Hillzkred@users.noreply.github.com>
2023-03-02 14:06:42 -0500

    upload 3D models on a map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useRef, useState } from "react";
-import { Source, Layer, Popup } from "react-map-gl";
-import MapContainer from "./components/MapContainer";
-import Csv from "./components/Csv";
-import centroid from "@turf/centroid";
-import { polygon } from "@turf/helpers";
+import { useRef, useState } from 'react';
+import { Source, Layer, Popup } from 'react-map-gl';
+import MapContainer from './components/MapContainer';
+import Csv from './components/Csv';
+import centroid from '@turf/centroid';
+import { polygon } from '@turf/helpers';
 
 export default function App() {
   const [data, setData] = useState(null);
@@ -48,38 +48,38 @@ export default function App() {
   };
 
   const buildingStyle = {
-    id: "buildings",
-    type: "fill",
+    id: 'buildings',
+    type: 'fill',
     paint: {
-      "fill-color": "#3a3a3a",
-      "fill-opacity": 0,
+      'fill-color': '#3a3a3a',
+      'fill-opacity': 0,
     },
   };
 
   const buildingHeight = {
-    id: "building-height",
-    type: "fill-extrusion",
+    id: 'building-height',
+    type: 'fill-extrusion',
     paint: {
-      "fill-extrusion-color": "#8a8a8a",
-      "fill-extrusion-opacity": 0.9,
-      "fill-extrusion-height": ["get", "Height"],
+      'fill-extrusion-color': '#8a8a8a',
+      'fill-extrusion-opacity': 0.9,
+      'fill-extrusion-height': ['get', 'Height'],
     },
   };
 
   return (
-    <div className="absolute top-0 left-0 h-full w-full">
+    <div className='absolute top-0 left-0 h-full w-full'>
       <nav>
-        <label className="fixed z-10 bg-emerald-500 hover:bg-emerald-700 text-white m-2 pr-2 pl-2 p-1 text-lg rounded-lg cursor-pointer">
+        <label className='fixed z-10 bg-emerald-500 hover:bg-emerald-700 text-white m-2 pr-2 pl-2 p-1 text-lg rounded-lg cursor-pointer'>
           Upload a GeoJSON file
           <input
-            className="hidden"
-            type="file"
+            className='hidden'
+            type='file'
             onChange={handleGeojsonUpload}
           />
         </label>
       </nav>
       <MapContainer handleClick={handleClick} ref={mapRef}>
-        <Source type="geojson" data={data}>
+        <Source type='geojson' data={data}>
           <Layer {...buildingStyle} />
           <Layer {...buildingHeight} />
         </Source>
--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,31 +1,58 @@
 import maplibregl from 'maplibre-gl';
+import DeckGL from '@deck.gl/react';
+import { ScenegraphLayer } from '@deck.gl/mesh-layers';
 import { forwardRef, useState } from 'react';
 import { Map } from 'react-map-gl';
 
 const MainMap = forwardRef(({ children, handleClick }, ref) => {
   const [cursor, setCursor] = useState('auto');
 
+  const layer = new ScenegraphLayer({
+    id: 'scenegraph-layer',
+    pickable: true,
+    data: [
+      {
+        name: 'Colma (COLM)',
+        address: '365 D Street, Colma CA 94014',
+        exits: 4214,
+        coordinates: [148.9819, -35.39847],
+      },
+    ],
+    scenegraph:
+      'https://maplibre.org/maplibre-gl-js-docs/assets/34M_17/34M_17.gltf',
+    getPosition: (d) => d.coordinates,
+    getOrientation: (d) => [0, Math.random() * 180, 90],
+    sizeScale: 1,
+    _lighting: 'pbr',
+  });
+
   return (
-    <Map
+    <DeckGL
       initialViewState={{
-        longitude: -75.72,
-        latitude: 45.18,
-        zoom: 10,
+        longitude: 148.9819,
+        latitude: -35.39847,
+        zoom: 18,
+        pitch: 60,
       }}
-      mapLib={maplibregl}
-      mapStyle='https://api.maptiler.com/maps/basic/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
-      onMouseEnter={() => {
-        setCursor('pointer');
-        console.log('Hovered');
-      }}
-      onMouseLeave={() => setCursor('auto')}
-      interactiveLayerIds={['buildings']}
-      cursor={cursor}
-      onClick={handleClick}
-      ref={ref}
+      controller={true}
+      layers={[layer]}
     >
-      {children}
-    </Map>
+      <Map
+        mapLib={maplibregl}
+        mapStyle='https://api.maptiler.com/maps/basic/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
+        onMouseEnter={() => {
+          setCursor('pointer');
+          console.log('Hovered');
+        }}
+        onMouseLeave={() => setCursor('auto')}
+        interactiveLayerIds={['buildings']}
+        cursor={cursor}
+        onClick={handleClick}
+        ref={ref}
+      >
+        {children}
+      </Map>
+    </DeckGL>
   );
 });
 export default MainMap;
